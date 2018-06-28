dfa06f5b559b0596995480c6daf41162487f60bd
Ollie Crook <olliejcrook@gmail.com>
2018-06-28 12:36:08 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/static', express.static('static'));
-app.set('view engine', 'hbs');
+app.use("/static", express.static("static"));
+app.set("view engine", "hbs");
 
 const menu = {
   1: {
     id: 1,
-    name: "Strawberry cheesecake",
+    name: "Plantain Tacos",
     price: 6
+  },
+  2: {
+    id: 2,
+    name: "Black Bean Quesadillas",
+    price: 5
+  },
+  3: {
+    id: 3,
+    name: "Guacamole",
+    price: 2.5
+  },
+  4: {
+    id: 4,
+    name: "Frijoles",
+    price: 3
+  },
+  5: {
+    id: 5,
+    name: "Chipotle Salsa",
+    price: 3
   }
 };
 
-app.get('/', function(req, res){
-  res.render('index');
+app.get("/", function(req, res) {
+  res.render("index");
 });
 
-app.listen(8080, function(){
-  console.log('Listening on port 8080');
+app.get("/api", function(req, res) {
+  res.json(menu);
+});
+
+app.listen(8080, function() {
+  console.log("Listening on port 8080");
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
+
+    this.state = {
+      menu: {}
+    };
   }
 
-  render(){
+  componentDidMount() {
+    console.log("HI");
+    fetch("//localhost:8080/api")
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        this.setState({
+          menu: json
+        });
+      })
+      .catch(error => alert("Sorry, I coulnd't find the menu"));
+  }
+
+  render() {
     return (
       <div>
-        Delivereat app
+        <h1>Delivereat app</h1>
+        <h2>Menu</h2>
+
+        {Object.values(this.state.menu).map(item => {
+          return (
+            <div>
+              <p>{item.name}</p>
+              <p>£{item.price}</p>
+            </div>
+          );
+        })}
       </div>
-    )
+    );
   }
 }
 
